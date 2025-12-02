a67754dc445240957fcd2457b9cd73c443c9a643
Adrian <adrian@MacBook-Air-Adrian-3.local>
2025-12-02 14:01:02 +0100

    MB Cockpit auto update 2025-12-02 14:01

--- src/app/knowledge-graph/page.tsx
+++ src/app/knowledge-graph/page.tsx
@@ -1,24 +1,20 @@
 "use client";
 
 import { useState } from "react";
-import KnowledgeGraph from "../../components/KnowledgeGraph";
-import ContentPane from "../../components/ContentPane";
+import KnowledgeGraph from "../components/KnowledgeGraph";
+import ContentPane from "../components/ContentPane";
 
 export default function KnowledgeGraphPage() {
   const [selectedNodeId, setSelectedNodeId] = useState<string | null>(null);
 
   return (
-    <div className="flex h-[calc(100vh-3rem)] bg-black text-white">
-      {/* LEFT: content */}
-      <div className="w-[40%] bg-neutral-900 border-r border-neutral-800">
-        <ContentPane selectedNodeId={selectedNodeId} />
-      </div>
-
-      {/* RIGHT: graph */}
-      <div className="flex-1 relative">
+    <div className="flex h-full">
+      <div className="flex-1">
         <KnowledgeGraph onSelectNode={setSelectedNodeId} />
+      </div>
+      <div className="w-[420px] border-l">
+        <ContentPane selectedNodeId={selectedNodeId} />
       </div>
     </div>
   );
 }
-
